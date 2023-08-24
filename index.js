1f9a74449ed3667a268e1a8bd7c6ba5734c7ee62
Shub1970 <18shubham1998@gmail.com>
2023-08-24 06:50:01 +0530

    video responsive

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const modal = document.querySelector(".modal");
-const close_button = document.querySelector(".close_button");
-const open_button = document.querySelector(".open_button");
+// const modal = document.querySelector(".modal");
+// const close_button = document.querySelector(".close_button");
+// const open_button = document.querySelector(".open_button");
 
-open_button.addEventListener("click", () => {
-  modal.showModal();
-  console.log("open");
-});
+// open_button.addEventListener("click", () => {
+//   modal.showModal();
+//   console.log("open");
+// });
 
-close_button.addEventListener("click", () => {
-  modal.close();
-  console.log("close");
-});
+// close_button.addEventListener("click", () => {
+//   modal.close();
+//   console.log("close");
+// });
